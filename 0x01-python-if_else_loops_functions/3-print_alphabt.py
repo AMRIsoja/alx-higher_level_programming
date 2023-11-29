16072f79ba4d6fd901553bfb1af2efa4b0782402
Abdulaziz@phone <abdulazizmuhammadrabiu@gmail.com>
2023-11-29 04:42:52 +0100

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
+    if alphabet == ord('e') or alphabet == ord('q'):
+        continue
     print("{:s}".format(chr(alphabet)), end="")
