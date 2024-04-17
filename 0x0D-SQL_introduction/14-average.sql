66ce0864c3fd200cbb9d04884bcdf2d5792e626d
ubuntu termux Techno Pouvoir 2 <abdulazizmuhammadrabiu@gmail.com>
2024-04-17 17:43:35 +0100

    changes

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- Calculates the average score of all the records in the table second table.
-SELECT AVG('score') AS 'average' FROM second_table;
+SELECT AVG('score') AS 'average' 
+FROM second_table;
