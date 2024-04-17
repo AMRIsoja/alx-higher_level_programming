1fdcf04e94fe83447a020c439d21913166325db4
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2024-04-17 10:05:45 +0100

    corrections

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,10 @@
 -- Display all values of the table
 
-SHOW * FROM first_table;
+-- Set the table name as a parameter
+SET @table_name = ?;
+
+-- Dynamically construct and execute the SQL query
+SET @query = CONCAT('SELECT * FROM ', @table_name);
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
