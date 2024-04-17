45162ea4f09cc380949d9200da4abb7a8c5c1d80
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2024-04-17 10:19:27 +0100

    corrections

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -2,9 +2,5 @@
 
 -- Set the table name as a parameter
 SET @table_name = ?;
-
--- Dynamically construct and execute the SQL query
-SET @query = CONCAT('SELECT * FROM ', @table_name);
-PREPARE stmt FROM @query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+USE hbtn_0c_0;
+SELECT * FROM @table_name;
